958177d8eb5446ba725e42d8fde22f64122a989a
Javier Suárez <ljsuarez7@gmail.com>
2024-07-26 12:41:25 +0200

    implementamos la directiva de erores

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,17 +7,61 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
+  @Input() set color(value: string){
+    this._color = value;
+    this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined){
+    this._errors = value;
+    this.setErrorMessage();
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
-    // console.log('constructor');
-    // console.log(el);
     this.htmlElement = el;
-
-    this.htmlElement.nativeElement.innerHTML = 'Hola mundo';
   }
 
   ngOnInit(): void {
-    console.log('on init');
+    this.setStyle();
+  }
+
+  setStyle(): void{
+    if(!this.htmlElement) return;
+    this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void{
+    if(!this.htmlElement) return;
+    if(!this._errors){
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    console.log(errors);
+
+
+    if(errors.includes('required')){
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+
+    if(errors.includes('minlength')){
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerText = `Minimo ${current}/${min} caracteres`;
+      return;
+    }
+
+    if(errors.includes('email')){
+      this.htmlElement.nativeElement.innerText = 'Este campo debe ser un email';
+      return;
+    }
+
+
   }
 
 }
