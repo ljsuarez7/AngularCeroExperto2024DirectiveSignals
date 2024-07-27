d6a262e631d30cadb4e59a620ffcedff7fb8b62c
Javier Suárez <ljsuarez7@gmail.com>
2024-07-27 16:02:25 +0200

    señal para contador

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  public counter = signal(10);
+  public squareCounter = computed( () => this.counter() * this.counter() );
+
+  increaseBy(value: number){
+    // this.counter.set(this.counter() + value);
+    this.counter.update(current => current + value);
+  }
+
 }
+
